7c803ff9b24f13b4635d95bc59f55ac6b9af35e4
ErikBaksay <79666150+ErikBaksay@users.noreply.github.com>
2022-01-07 14:59:16 +0100

    Welcome page improved, Images Compressed

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,9 @@
+import { amounts_in_cart } from './../../cart_data';
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { products } from './../../products';
+import { product } from "./../../product";
+import { items_in_cart } from 'src/app/cart_data';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +12,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  products : product[] = products
+
   constructor(public router: Router) { }
 
   goToPage(page:any){
     this.router.navigate([page])
+  }
+
+  addProductToCart(id:number){
+    if (items_in_cart.includes(id)){
+      let indexOfItem = items_in_cart.indexOf(id)
+      amounts_in_cart[indexOfItem] ++
+      
+    }else{
+      items_in_cart.push(id)
+      amounts_in_cart.push(1)
+    }
   }
 
   ngOnInit(): void {
